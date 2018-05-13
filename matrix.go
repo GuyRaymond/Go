0ccda9248f3ece7b5ba2cf30fe2cab2cb92c4d8c
Guy Raymond BESSENG A IREH <guy.besseng@gmail.com>
2018-05-14 01:35:55 +0200

    Update matrix.go

--- matrix.go
+++ matrix.go
@@ -1,8 +1,18 @@
 package matrix
 % Some useful algorithm for matrix
 import (
-    "sort"
+    gosort"sort"
 )
+
+type pair struct {
+	x float64
+	i int
+}
+type slicePair []pair
+
+func (ps slicePair) Len() int      { return len(ps) }
+func (ps slicePair) Swap(i, j int) { ps[j], ps[i] = ps[i], ps[j] }
+func (ps slicePair) Less(i, j int) bool { return ps[i].x < ps[j].x || ps[i].i < ps[j].i }
 
 func sum(xs []float64) float64 {
     val ans float64 = 0;
@@ -24,8 +34,18 @@ func product(xs []float64) float64 {
 func dot(x,y []float64) float64 {
 }
 
-func sort(xs []float64) (float64[],uint[]) {
-    ys := make [][2]float64
-    for i,x := range xs {
-    }
+func sort(xs []float64) (ys []float64, zs []int) {
+	var us slicePair = make(slicePair, len(xs))
+	var n int = len(xs)
+	for i, x := range xs {
+		us[i] = pair{x: x, i: i}
+	}
+	gosort.Sort(us)
+	ys = make([]float64, n)
+	zs = make([]int, n)
+	for i, u := range us {
+		ys[i] = u.x
+		zs[i] = u.i
+	}
+	return
 }
